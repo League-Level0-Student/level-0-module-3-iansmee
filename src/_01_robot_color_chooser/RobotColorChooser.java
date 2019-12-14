6650240c4188014cc0d5d95bdab88af0c04ff494
League Student <league@iMac-22.attlocal.net>
2019-12-14 11:25:54 -0800

    robot color chooser

--- src/_01_robot_color_chooser/RobotColorChooser.java
+++ src/_01_robot_color_chooser/RobotColorChooser.java
@@ -12,27 +12,241 @@ import java.awt.Color;
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		//1. Create a new Robot
-		 
+	
 		
 	Robot rob = new Robot();
+	for(int i=0;i<10;i++) {
 		//3. Ask the user what color they would like the robot to draw
-		String color = JOptionPane.showInputDialog("what color do you like");
+		String color = JOptionPane.showInputDialog("what color would you like");
 		//5. Use an if/else statement to set the pen color that the user requested
+if( color.equals("blue") ) {
+	rob.setPenColor(Color.blue);
+}
+if( color.equals("red") ) {
+	rob.setPenColor(Color.red);
+}
+if( color.equals("green") ) {
+	rob.setPenColor(Color.green);
+}
+
 
         //6. If the user doesn’t enter anything, choose a random color
-
+if(color.equals("")) {
+	rob.setRandomPenColor();
+}
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 		//4. Set the pen width to 10
 		rob.setPenWidth(10);
+		rob.penDown();
 	    //2. Make the robot draw a shape (this will take more than one line of code)
-	rob.setSpeed(100);
+	rob.setSpeed(1000);
 rob.move(100);
 rob.turn(90);
 rob.move(100);
 rob.turn(90);
 rob.move(100);
 rob.turn(90);
+rob.move(200);
 rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(200);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(200);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(200);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(200);
+rob.turn(90);
+rob.move(300);
+rob.turn(90);
+rob.move(300);rob.turn(90);
+rob.move(300);rob.turn(90);
+rob.move(300);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(200);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(200);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(200);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(200);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(200);
+rob.turn(90);
+rob.move(300);
+rob.turn(90);
+rob.move(300);rob.turn(90);
+rob.move(300);rob.turn(90);
+rob.move(300);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(200);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(200);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(200);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(200);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(200);
+rob.turn(90);
+rob.move(300);
+rob.turn(90);
+rob.move(300);rob.turn(90);
+rob.move(300);rob.turn(90);
+rob.move(300);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(200);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(200);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(200);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(200);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(200);
+rob.turn(90);
+rob.move(300);
+rob.turn(90);
+rob.move(300);rob.turn(90);
+rob.move(300);rob.turn(90);
+rob.move(300);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(200);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(200);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(200);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(200);
+rob.turn(90);
+rob.move(100);
+rob.turn(90);
+rob.move(200);
+rob.turn(90);
+rob.move(300);
+rob.turn(90);
+rob.move(300);rob.turn(90);
+rob.move(300);rob.turn(90);
+rob.move(300);
+rob.turn(90);
+rob.move(200);
+rob.turn(270);
+rob.move(100);
+rob.turn(270);
+rob.move(500);
+rob.turn(270);
+rob.move(500);
+rob.turn(270);
+rob.move(500);
+rob.turn(270);
+rob.move(500);
 	}
+}
 }
