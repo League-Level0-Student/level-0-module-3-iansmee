6eaa7bcddd2e820bd2e155695cb10261380af3c7
root <root@d82f63fe42bf>
2020-03-14 18:31:19 +0000

    obedient robot

---
+++ src/_16_obedient_robot/obedient_robot.java
@@ -0,0 +1,29 @@
+package _16_obedient_robot;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class obedient_robot {
+public static void main(String[] args) {
+	
+	Robot bob = new Robot();
+	
+	bob.setSpeed(100);
+	bob.penDown();
+	
+	bob.move(90);
+	bob.turn(90);
+	bob.move(90);
+	bob.turn(90);
+	bob.move(90);
+	bob.turn(90);
+	bob.move(90);
+	bob.turn(90);
+	
+	
+	
+	
+	
+	
+	
+}
+}
